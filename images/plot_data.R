761f31b0e2af63632be004509b961d8f85ac80a7
Adam K <adamkunkel54@gmail.com>
2024-06-19 14:20:43 -0400

    updating images

--- images/plot_data.R
+++ images/plot_data.R
@@ -5,7 +5,7 @@
 #### Set Libraries, read in data ####
 library(tidyverse); library(ggstream); library(lubridate); library(sf); 
 library(rstatix); library(ggpubr); library(ggExtra); library(ggridges)
-library(viridis); library(hrbrthemes)
+library(viridis); library(hrbrthemes); library(tidyquant)
 
 # set up environment
 options(scipen = 999) # turn off scientific notation
@@ -49,19 +49,18 @@ ggplot(a, aes(x = `fatalities`, y = `act`, fill = stat(x))) +
     panel.spacing = unit(0.1, "lines"),
     strip.text.x = element_text(size = 14),
     axis.title.x = element_text(size = 14, hjust = 0.4),
-    axis.text = element_text(size = 16),
-    axis.title.y = element_text(size = 16),
+    axis.text.x = element_text(size = 16),
+    axis.text.y = element_text(size = 16),
     plot.caption = element_text(size = 12)
   )
 dev.off()
 
 #### scatter plot of violence since 2021-11-01 #####
-rm(list=ls())
-a = read.csv("./data/Kunkel-Ellis-final.csv") %>%
+rm(list = ls())
+a = read.csv("../../car_wagner_analysis/data/Kunkel-Ellis-final.csv") %>%
   mutate(event_date = ymd(event_date)) %>%
-  mutate(event_date = floor_date(event_date, "week"))
-a$wagner = "State"
-a$wagner[a$t_ind == 1] = "Wagner"
+  mutate(event_date = floor_date(event_date, "week"),
+         wagner = ifelse(t_ind == 1, "Wagner", "State"))
 date = rep(ymd("2021-11-01"), nrow(a))
 a$score = date - a$event_date
 d = a %>%
@@ -71,9 +70,8 @@ d = a %>%
   summarize(death = mean(death), fatalities = sum(fatalities)) %>%
   as.data.frame()
 
-library(tidyquant)
-death = 
-  ggplot(d) + 
+svg("./car_1.svg")
+ggplot(d) + 
   geom_point(aes(x = score, y = fatalities, colour = wagner)) +
   geom_vline(xintercept = 0, linetype = "longdash", color = "black") +
   geom_ma(ma_fun = SMA, n = 14, aes(x = score, y = fatalities, 
@@ -81,9 +79,126 @@ death =
   xlab("Days Before and After Nov. 1, 2021") + ylab("Fatalities") +
   labs(colour = "Actor") + guides(linetype = "none") + ylim(0,120) +
   scale_color_manual(labels = c("State Forces", "Wagner"), values = c("#A52A2A", "#000000")) +
-  theme_pubr()
+  theme_pubr() +
+  theme(
+    plot.title = element_text(size = 20),
+    panel.spacing = unit(0.1, "lines"),
+    strip.text.x = element_text(size = 14),
+    axis.title.x = element_text(size = 14, hjust = 0.4),
+    axis.text.x = element_text(size = 16),
+    axis.text.y = element_text(size = 16),
+    plot.caption = element_text(size = 12),
+    legend.text = element_text(size = 12),
+    legend.key.size = unit(1.5, "lines")
+  )
+dev.off()
 
-# with marginal histogram
-pdf("./results/death_scatter.pdf")
-death
+
+###### Who Keeps the Peace ######
+rm(list = ls())
+a = readRDS("../../who_keeps/data/kunkel_which_pks.rds") %>% 
+  as.data.frame()
+
+df = a %>%
+  group_by(prio.grid) %>%
+  summarize(f_pko_deployed = sum(radpko_f_pko_deployed), m_pko_deployed = sum(radpko_m_pko_deployed), 
+            violence = sum(ucdp_reb_vac_all, ucdp_gov_vac_all))
+
+# change 0s to NA to make plot prettier
+df$f_pko_deployed[df$f_pko_deployed == 0] <- NA
+df$m_pko_deployed[df$m_pko_deployed == 0] <- NA
+df$violence[df$violence == 0] <- NA
+
+# restructure the data so grids can be duplicated and pko/violence is on the same scale 
+# and named the same variable
+df.pk_f = subset(df, f_pko_deployed > 0) %>%
+  select(-c("violence", "m_pko_deployed")) %>%
+  rename(count = f_pko_deployed)
+df.pk_f$ct.type = "Women PKs Deployed"
+df.pk_m = subset(df, m_pko_deployed > 0) %>%
+  select(-c("violence", "f_pko_deployed")) %>%
+  rename(count = m_pko_deployed)
+df.pk_m$ct.type = "Men PKs Deployed"
+df.vo = subset(df, violence > 0) %>%
+  select(-c("m_pko_deployed", "f_pko_deployed")) %>%
+  rename(count = violence)
+df.vo$ct.type = "Violent Deaths"
+
+# rejoin to same column
+dd = rbind(df.pk_f, df.pk_m, df.vo)
+
+rm(list = setdiff(ls(), c("dd", "df"))) 
+gc()
+
+### MERGE ACLED DATA WITH PRIO GRID IDS ###
+prio_shp <- st_read(dsn = "../../who_keeps/data/prio", layer = "priogrid_cell", # get prio shapefiles
+                    stringsAsFactors = F)
+afr_shp <- st_read(dsn = "../../who_keeps/data/gadm/africa", layer = "afr_g2014_2013_0", # get Africa shapefiles
+                   stringsAsFactors = F)
+
+### save the CRS
+proj_crs <- st_crs(prio_shp)
+dd$gid = dd$prio.grid
+dd$prio.grid = NULL
+df$gid = df$prio.grid
+df$prio.grid = NULL
+# convert, get rid of useless data
+df.prio = left_join(df, prio_shp, by = "gid") %>%
+  select(-c(2:7))
+df = left_join(df, prio_shp, by = "gid") %>%
+  as.data.frame() %>%
+  select(-c("geometry", "col", "row"))
+dd_ac = left_join(dd, prio_shp, by = "gid") %>%
+  as.data.frame() %>%
+  select(-c("geometry", "col", "row"))
+df_ac= df %>%
+  drop_na(violence) # drop NAs
+df_pk = df %>%
+  drop_na(f_pko_deployed, m_pko_deployed)
+
+# plot of variables as different colors and different shape
+
+
+dsc.wom =
+  ggplot(afr_shp) + geom_sf(aes(geometry = geometry), alpha = 0.3,fill = NA) + # e5695b
+  geom_point(data = df, aes(x = xcoord, y = ycoord, size=f_pko_deployed, colour = "#9E314B"), alpha=0.5, shape = 19) +
+  scale_fill_viridis_c(option="E") +
+  scale_size(range = c(.1, 15), name="Count", labels = c("10,000", "20,000"), breaks = c(10000, 20000)) +
+  theme_void()
+
+dsc_wom = dsc.wom + 
+  labs(colour = "Variable") + 
+  scale_color_manual(labels = c("Women PKs Deployed"), values = c("#9E314B")) +
+  theme(legend.background = element_rect(color = "black"), legend.position = c(0.25, 0.2),
+        plot.margin = unit(c(0,0,0,0), "cm"), legend.margin=margin(c(2,2,2,2)), 
+        legend.key.size = unit(2, 'cm'),
+        legend.text = element_text(size = 16)
+        ) + 
+  guides(shape = guide_legend(order = 1),col = guide_legend(order = 2), legend.direction="vertical") +
+  xlim(-14,37) + ylim(-12,21)
+
+svg("./women_map.svg", width = 10, height = 7)
+dsc_wom
+dev.off()
+
+dsc.men =
+  ggplot(afr_shp) + geom_sf(aes(geometry = geometry), alpha = 0.3,fill = NA) +
+  geom_point(data = df, aes(x = xcoord, y = ycoord, size=m_pko_deployed, colour = "#EB5307"), alpha=0.5, shape = 19) +
+  scale_fill_viridis_c(option="E") +
+  scale_size(range = c(.1, 24), name="Count", labels = c("250,000", "500,000"), breaks = c(250000, 500000)) +
+  theme_void()
+
+dsc_men = dsc.men + 
+  labs(colour = "Variable") + 
+  scale_color_manual(labels = c("Men PKs Deployed"), values = c("#EB5307")) +
+  theme(legend.background = element_rect(color = "black"), legend.position = c(0.25, 0.18),
+        plot.margin = unit(c(0,0,0,0), "cm"), legend.margin=margin(c(5,5,5,5)), 
+        legend.key.size = unit(0.2, 'cm'),
+        legend.text = element_text(size = 12)
+        ) + 
+  guides(shape = guide_legend(order = 1),col = guide_legend(order = 2), legend.direction="vertical") +
+  xlim(-14,37) + ylim(-12,21)
+
+svg("./results/men_map.svg", width = 10, height = 7)
+dsc_men
 dev.off()
