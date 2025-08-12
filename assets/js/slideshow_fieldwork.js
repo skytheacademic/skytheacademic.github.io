f7c3efaa91b51ca375c3b07ccca35c7e42b1aa65
Adam K <adamkunkel54@gmail.com>
2025-08-12 15:24:19 -0400

    seems likely to have been a directory issue 🙃

--- assets/js/slideshow_fieldwork.js
+++ assets/js/slideshow_fieldwork.js
@@ -4,17 +4,13 @@
 function startSlideshow() {
   let currentIndex = 0;
   const images = [
-    './images/car.svg',
-    './images/drc_ug.svg',
-    './images/car_1.svg',
-    './images/map_prop.svg'
-    // './images/fiji_1.svg',
-    // './images/fiji_2.jpg',
-    // './images/fiji_3.jpg',
-    // './images/fiji_4.jpg',
-    // './images/fiji_5.jpg',
-    // './images/fiji_6.jpg',
-    // './images/fiji_7.jpg'
+    './images/fiji_1.svg',
+    './images/fiji_2.jpg',
+    './images/fiji_3.jpg',
+    './images/fiji_4.jpg',
+    './images/fiji_5.jpg',
+    './images/fiji_6.jpg',
+    './images/fiji_7.jpg'
   ];
 
   const imageElement = document.getElementById('slideshow-image');
